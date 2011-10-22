eb7bc955851a68a8008c675485c237e544453f3c
Kazuhiro Shibuya <stevenlabs@gmail.com>
2011-10-23 00:51:46 +0900

    Added input view dispatcher.

--- lib/MyWord/Web/Dispatcher.pm
+++ lib/MyWord/Web/Dispatcher.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::Lite;
+use MyWord::API::Store;
 
 any '/' => sub {
     my ($c) = @_;
@@ -14,5 +15,41 @@ post '/account/logout' => sub {
     $c->session->expire();
     $c->redirect('/');
 };
+
+any '/input' => sub {
+    my ($c) = @_;
+    $c->render('input.tt');
+};
+
+post '/input/store' => sub {
+    my ($c) = @_;
+    my $word = $c->req->param('word');
+    my $itr = MyWord::API::Store->is_there($word, $c);
+    my $data = undef;
+    if (my $row = $itr->next) {
+	$data = MyWord::API::Store->form($row);
+    } else {
+	my $res = MyWord::API::Store->store($word, $c);
+	if (defined $res) {
+	    $data = MyWord::API::Store->form($res);
+	}
+    }
+
+    $c->render('input.tt', {res => $data});
+};
+
+any '/view' => sub {
+    my ($c) = @_;
+    my $page = 1;
+    my $num = 10;
+    my ($rows, $pager) = $c->db->search_with_pager(
+	'words' => {}, {page => $page, rows => $num});
+    my @datas;
+    foreach my $row (@{$rows}) {
+	push @datas, MyWord::API::Store->form($row);
+    }
+    $c->render('view.tt' => { words => \@datas });
+};
+
 
 1;
