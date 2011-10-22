3cab29840dd48796ea80fc49ccf85e1c2fedb457
Kazuhiro Shibuya <stevenlabs@gmail.com>
2011-10-23 00:52:16 +0900

    Added words table.

--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -2,3 +2,9 @@ CREATE TABLE IF NOT EXISTS sessions (
     id           CHAR(72) PRIMARY KEY,
     session_data TEXT
 );
+
+CREATE TABLE IF NOT EXISTS words (
+       word     TEXT,
+       japanese TEXT,
+       english  TEXT
+);
